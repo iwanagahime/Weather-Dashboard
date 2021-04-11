0a33cf514341f5e46310a28c6d63067b6ef9da19
iwanagahime <eliza.krucon@gmail.com>
2021-04-11 21:21:24 +0100

    render styled current day weather card

--- assets/main.js
+++ assets/main.js
@@ -27,11 +27,25 @@ const getDataByCityName = async (event) => {
   if (target.is("li")) {
     const cityName = target.data("city");
 
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`;
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=${API_KEY}`;
 
     const data = await fetchData(url);
-    console.log(data);
+
+    const currentDayData = transformData(data);
+
+    renderCurrentDayCard(currentDayData);
   }
+};
+
+const transformData = (data) => {
+  return {
+    cityName: data.name,
+    temperature: data.main.temp,
+    humidity: data.main.humidity,
+    windSpeed: data.wind.speed,
+    date: moment.unix(data.dt).format("MM/DD/YYYY"),
+    iconURL: `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,
+  };
 };
 
 const onSubmit = async (event) => {
@@ -48,11 +62,13 @@ const onSubmit = async (event) => {
 
   $("#city-input").val("");
 
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`;
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=${API_KEY}`;
 
   const data = await fetchData(url);
 
-  console.log(data);
+  const currentDayData = transformData(data);
+
+  renderCurrentDayCard(currentDayData);
 };
 
 const renderCitiesFromLocalStorage = () => {
@@ -68,7 +84,6 @@ const renderCitiesFromLocalStorage = () => {
       .attr("data-city", city)
       .text(city);
 
-    // li.on("click", onClick);
     ul.append(li);
   };
 
@@ -79,8 +94,27 @@ const renderCitiesFromLocalStorage = () => {
   $("#searched-cities").append(ul);
 };
 
+const renderCurrentDayCard = (data) => {
+  $("#current-day-card").empty();
+
+  const card = `<div class="card">
+  <div class="card-body">
+    <h2>
+      ${data.cityName} (${data.date}) <img src="${data.iconURL}"/>
+    </h2>
+    <div class="py-2">Temperature: ${data.temperature} &deg; F</div>
+    <div class="py-2">Humidity: ${data.humidity}%</div>
+    <div class="py-2">Wind Speed: ${data.windSpeed} MPH</div>
+    <div class="py-2">UV Index 45</div>
+  </div>
+</div>`;
+
+  $("#current-day-card").append(card);
+};
+
 const onReady = () => {
   renderCitiesFromLocalStorage();
 };
+
 $("#search-by-city-form").on("submit", onSubmit);
 $(document).ready(onReady);
