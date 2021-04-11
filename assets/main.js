5fc9101b9a49569fb14df2c3b2bc71fedd9e25f5
iwanagahime <eliza.krucon@gmail.com>
2021-04-11 11:14:23 +0100

    save to local storage on submit

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,21 @@
+const getFromLocalStorage = () => {
+  const localStorageData = JSON.parse(localStorage.getItem("cities"));
+
+  if (localStorageData !== null) {
+    return localStorageData;
+  } else {
+    return [];
+  }
+};
 const onSubmit = (event) => {
   event.preventDefault();
-  console.log("onSubmit");
+
+  const cityName = $("#city-input").val();
+
+  const cities = getFromLocalStorage();
+
+  cities.push(cityName);
+  console.log(cities);
+  localStorage.setItem("cities", JSON.stringify(cities));
 };
 $("#search-by-city-form").on("submit", onSubmit);
